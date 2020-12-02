3000d2c5325895fc8faef1e4db08c050bf9ebaed
belikeahsan <ahsan@quicklinktele.net>
2020-12-02 18:54:56 +0500

    isntall jetstream

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-    purge: [],
-    darkMode: "class", // or 'media' or 'class'
+    purge: [
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
     theme: {
-        extend: {}
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
+
     variants: {
-        extend: {}
+        opacity: ['responsive', 'hover', 'focus', 'disabled'],
     },
-    plugins: []
+
+    plugins: [require('@tailwindcss/ui')],
 };
